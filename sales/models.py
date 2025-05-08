2039529f43cafe0e85cb2b731998e11b69343755
JonasMahlburg <darrylmanga@yahoo.com>
2025-05-08 14:36:56 +0200

    added divers Models to show relationships

--- sales/models.py
+++ sales/models.py
@@ -7,3 +7,23 @@ class Customer(models.Model):
   newsletter_abo = models.BooleanField(default=True)
   email_address = models.EmailField(max_length=30, blank=True, default="")
   account = models.FloatField(blank=True, null=True)
+
+
+class Product(models.Model):
+  name = models.CharField(max_length=30)
+  price = models.FloatField()
+
+class Bill(models.Model):
+  total_amount = models.FloatField()
+  is_paid = models.BooleanField(default=False)
+
+class Order(models.Model):
+  customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+  products = models.ManyToManyField(Product, through="Producttype")
+  bill = models.OneToOneField(Bill, on_delete=models.CASCADE)
+
+
+class Producttype(models.Model):
+  order = models.ForeignKey(Order, on_delete=models.CASCADE)
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+  type_name = models.CharField(max_length=300)
