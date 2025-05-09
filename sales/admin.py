574ff4d094021ebd26c179fb34438a2d7899231b
JonasMahlburg <darrylmanga@yahoo.com>
2025-05-09 10:09:13 +0200

    expending admin panel

--- sales/admin.py
+++ sales/admin.py
@@ -3,7 +3,24 @@ from .models import Customer, Product, Producttype, Order, Bill
 
 class CustomerAdmin(admin.ModelAdmin):
     list_filter=["first_name", "last_name"]
-    list_display=["first_name", "account"]
+    list_display=["first_name", "last_name"]
+    readonly_fields=["account"]
+    prepopulated_fields={"slug":["first_name", "last_name"]}
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": ["first_name", "last_name", "account"],
+            },
+        ),
+        (
+            "Advanced options",
+            {
+                "classes": ["collapse"],
+                "fields": ["newsletter_abo", "slug"],
+            },
+        ),
+    ]
 
 
 # Register your models here.
