046beb3db11502c5764c7c58f64163ded5be6834
JonasMahlburg <darrylmanga@yahoo.com>
2025-05-12 14:45:32 +0200

    added Detail.view

--- sales/views.py
+++ sales/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Customer
+from django.utils import timezone
 # Create your views here.
 
 class CustomerListView(ListView):
@@ -12,3 +14,12 @@ class CustomerListSearchView(CustomerListView):
     def get_queryset(self):
         name = self.kwargs.get("name")
         return Customer.objects.filter(first_name=name)
+
+class CustomerDetailView(DetailView):
+     model = Customer
+     template_name = "sales/detail.html"
+
+     def get_object(self):
+         obj = super().get_object()
+         obj.last_accessed = timezone.now()
+         return obj
