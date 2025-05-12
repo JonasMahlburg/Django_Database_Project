a8d48f743655ee1e7787cf24cf0afd1ee9e0e441
JonasMahlburg <darrylmanga@yahoo.com>
2025-05-12 12:49:01 +0200

    added template/sales with list.view

---
+++ sales/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import CustomerListView, CustomerListSearchView
+
+urlpatterns = [
+    path('', CustomerListView.as_view()),
+    path('<str:name>/', CustomerListSearchView.as_view())
+ 
+]
--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from .models import Customer
 # Create your views here.
+
+class CustomerListView(ListView):
+    model = Customer
+    template_name = "sales/list.html"
+    paginate_by = 2
+
+class CustomerListSearchView(CustomerListView):
+    def get_queryset(self):
+        name = self.kwargs.get("name")
+        return Customer.objects.filter(first_name=name)
